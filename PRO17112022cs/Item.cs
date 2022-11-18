810626aaa17f364f4c4b9a386288919093c5cddb
yarinnissan1994 <yarinnissan1994@gmail.com>
2022-11-18 15:26:08 +0200

    home work update

--- PRO17112022cs/Item.cs
+++ PRO17112022cs/Item.cs
@@ -36,26 +36,66 @@ namespace PRO17112022cs
                 Console.WriteLine("#{0}", itemsArr2[i].Id);
                 Console.WriteLine("Item Name: " + itemsArr2[i].Name);
             }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
             Item[] itemsArr3 = new Item[10];
             Random rand = new Random();
             for (int i = 0; i < 10; i++)
             {
                 if (rand.Next(1,3) == 1)
                 {
-                    Console.WriteLine("#{0} Drink - Please enter a liquid color: ", i+1);
+                    Console.WriteLine("#{0} Drink", i+1);
+                    Console.WriteLine("Please enter drink name: ");
+                    string drinkNameInput = Console.ReadLine();
+                    Console.WriteLine("Please enter liquid color: ");
                     string liquidColorInput = Console.ReadLine();
+                    Console.WriteLine("Please enter price: ");
+                    int drinkPriceInput = int.Parse(Console.ReadLine());
                     itemsArr3[i] = new Drink(i + 1, liquidColorInput);
+                    itemsArr3[i].Name = drinkNameInput;
+                    itemsArr3[i].Price = drinkPriceInput;
                 }
                 else
                 {
-                    Console.WriteLine("#{0} Food - Please enter a food weight: ", i + 1);
+                    Console.WriteLine("#{0} Food", i + 1);
+                    Console.WriteLine("Please enter food name: ");
+                    string foodNameInput = Console.ReadLine();
+                    Console.WriteLine("Please enter food weight in grams: ");
                     int weightInput = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Please enter price: ");
+                    int foodPriceInput = int.Parse(Console.ReadLine());
                     itemsArr3[i] = new Food(i + 1, weightInput);
+                    itemsArr3[i].Name = foodNameInput;
+                    itemsArr3[i].Price = foodPriceInput;
                 }
             }
             for (int i = 0; i < itemsArr3.Length; i++)
             {
                 Console.WriteLine("item ID = {0}", itemsArr3[i].Id);
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            for (int i = 0; i < itemsArr3.Length; i++)
+            {
+                if (itemsArr3[i] is Drink)
+                {
+                    Drink drink = (Drink)itemsArr3[i];
+                    Console.WriteLine("#{0} Drink: " + drink.Name, drink.Id);
+                    Console.WriteLine(drink.LiquidColor);
+                    Console.WriteLine("{0}$", drink.Price);
+                    Console.WriteLine();
+
+                }
+                else if(itemsArr3[i] is Food)
+                {
+                    Food food = (Food)itemsArr3[i];
+                    Console.WriteLine("#{0} Food:" + food.Name , food.Id);
+                    Console.WriteLine("{0}grams", food.Weight);
+                    Console.WriteLine("{0}$", food.Price);
+                    Console.WriteLine();
+                }
             }
         }
         
